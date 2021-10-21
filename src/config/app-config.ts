27da1b305165159553df3b231ae1f2d05715d93e
Jairo Rodrigues <bbm.almoxarifado@gmail.com>
2021-10-21 16:57:52 -0300

    Alterado url do site

--- src/config/app-config.ts
+++ src/config/app-config.ts
@@ -2,4 +2,4 @@ export const API_URL = 'https://secret-atoll-67581.herokuapp.com';
 export const POSTS_URL = `${API_URL}/posts`;
 
 export const SITE_NAME = 'Jairo Rodrigues';
-export const SITE_URL = 'https://clever-bardeen-b9bd84.netlify.app';
+export const SITE_URL = 'http://jrprojetosweb.net';
